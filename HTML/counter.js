500630859a080a556bcce17d0f047e27c57dfe62
SanaWaqas <swaqas@myhoopoe.com>
2021-07-29 09:54:02 +0500

    revised programming lesson, added few information, practiced for loop and assignment done in counter.js

--- HTML/counter.js
+++ HTML/counter.js
@@ -17,3 +17,10 @@ function printDesiredTable (userChoice, userLimit) {
     console.log(userChoice + ' X ' + i + ' = ' + (userChoice * i))
   }
 }
+
+var letters = ["a", "b" , "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+function printLetters () {
+  for (i=0; i<=25; i++) {
+    console.log(letters[i])}
+  }
