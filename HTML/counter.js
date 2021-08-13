e21f738bcce567dbe9c3d9d98d86b00f0032e3ca
Abid Mahmood <abidmahmoodbutt@hotmail.com>
2021-08-13 20:02:42 +0500

    Nested loops practice.

--- HTML/counter.js
+++ HTML/counter.js
@@ -25,3 +25,79 @@ function printLetters () {
     console.log(letters[i])}
   }
 
+
+
+// Nested Loops (FOR)
+
+// Dry-run
+
+/*
+  i = 0; true; [j = 0; true, j = 1, true, .... j = 10; false;]
+  i = 1; true; [j = 0; true, j = 1, true, .... j = 10; false;]
+
+  |1  2  3|
+  |4  5  6|
+  |7  8  9|
+
+  [1][1]
+
+  a = [1][1]
+  b = [2][1]
+  c = [2][2]
+  .
+  .
+  .
+  o = [5][5]
+
+  a
+  bc 
+  def
+  ghij
+  klmno
+
+  *
+  **
+  ***
+  ****
+  *****
+
+  *****
+  ****
+  ***
+  **
+  *
+
+  i = 1; j MUST HAVE 1 iteration
+  i = 2; j MUST HAVE 2 iteration
+  .
+  .
+  .
+  i = 5; j MUST HAVE 5 iteration
+
+  \n => newline
+  \r => Carriage return
+  \n
+
+  i=0; j=0;
+  i=0; j=1;
+  i=0; j=2;
+  .
+  i=0; j=4;
+
+  i=1; j=0;
+  .
+  .
+  i=1; j=4;
+  .
+  .
+  .
+  i=4; j=4;
+
+  i=5; j=0;
+*/
+
+for (i=0; i<5; i++) {
+  for (j=0; j<5; j++) {
+    console.log('*');
+  }
+}
