76b1ef23fcfa7e6458df9c03ea11e193b2e645b7
SanaWaqas <swaqas@myhoopoe.com>
2021-08-17 13:51:15 +0500

    nested loop assignment done

--- HTML/counter.js
+++ HTML/counter.js
@@ -96,8 +96,55 @@ function printLetters () {
   i=5; j=0;
 */
 
-for (i=0; i<5; i++) {
-  for (j=0; j<5; j++) {
-    console.log('*');
+for (i=1; i<=5; i++) {
+  for (j=1; j<=i; j++) {
+    document.write('*');
   }
+  document.write('<br>');
 }
+
+/* dry run
+
+i=1; true; j=1 true; print *; break;
+i=2; true; j=1 true; print *;
+i=2; true; j=2 true; print *; break;
+i=3; true; j=1 true; print *;
+i=3; true; j=2 true; print *;
+i=3; true; j=3 true; print *; break;
+i=4; true; j=1 true; print *;
+i=4; true; j=2 true; print *;
+i=4; true; j=3 true; print *;
+i=4; true; j=4 true; print *; break;
+i=5; true; j=1 true; print *;
+i=5; true; j=2 true; print *;
+i=5; true; j=3 true; print *;
+i=5; true; j=4 true; print *;
+i=5; true; j=5 true; print *; break;
+
+*/
+
+for (i=5; i>=1; i--) {
+  for (j=i; j>=1; j--) {
+    document.write('*');
+  }
+  document.write('<br>');
+}
+
+/* 
+i=5; true; j=5 true; print *; 
+i=5; true; j=4 true; print *;
+i=5; true; j=3 true; print *;
+i=5; true; j=2 true; print *;
+i=5; true; j=1 true; print *; break;
+i=4; true; j=4 true; print *; 
+i=4; true; j=3 true; print *;
+i=4; true; j=2 true; print *;
+i=4; true; j=1 true; print *; break;
+i=3; true; j=3 true; print *; 
+i=3; true; j=2 true; print *;
+i=3; true; j=1 true; print *; break;
+i=2; true; j=2 true; print *;
+i=2; true; j=1 true; print *; break
+i=1; true; j=1 true; print *; break;
+
+*/
