f23f8bacd466eada61ae1f3255af562a5d20947f
Abid Mahmood <abidmahmoodbutt@hotmail.com>
2021-08-17 23:04:30 +0500

    Conditional Statements (IF, IF .. ELSE, IF .. ELSE .. IF)

---
+++ HTML/conditional-statements-if.js
@@ -0,0 +1,63 @@
+/**
+ * IF
+ * Syntax:
+      if (condition) {
+        // statements
+      }
+
+ * IF .. ELSE
+ * Syntax:
+      if (condition) {
+
+      } else {
+
+      }
+  
+  * IF .. ELSE .. IF
+  * Syntax:
+      if (condition) {
+
+      } else if (condition) {
+
+      } else {
+
+      }
+ */
+
+ i = 0;
+ if (i == 2) {
+   document.write('Wow! it is 2')
+ }
+
+ i = 0;
+ if (i == 2) {
+  document.write('Wow! it is 2')
+ } else {
+  document.write('Ooops! it\'s not 2')
+ }
+
+ i = 0;
+ if (i == 1) {
+  document.write('It is 1!') 
+ } else if (i == 2) {
+  document.write('Wow! it is 2')
+ } else if (i == 3) {
+  document.write('Hey, it is 3!')
+ } else {
+  document.write('Ooops! it\'s something else')
+ }
+
+ i = 0;
+ if (i == 1) {
+  document.write('It is 1!') 
+ }
+
+ if (i == 2) {
+  document.write('Wow! it is 2')
+ }
+ 
+ if (i == 3) {
+  document.write('Hey, it is 3!')
+ } else {
+  document.write('Ooops! it\'s something else')
+ }
