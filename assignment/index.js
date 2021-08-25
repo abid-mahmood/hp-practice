ff0787e51ab051901ba134371b27b2e814ee0b39
SanaWaqas <swaqas@myhoopoe.com>
2021-08-25 12:52:31 +0500

    assignment else if

---
+++ assignment/index.js
@@ -0,0 +1,31 @@
+
+if (grade >= 90 && grade <= 100) {
+	document.write("Excellent! You achieved A+ grade")
+}
+else if (grade >= 85 && grade <= 89) {
+	document.write("Great! You achieved A- grade")
+}
+else if (grade >= 80 && grade <= 84) {
+	document.write("Well done! You achieved A grade")
+}
+else if (grade >= 75 && grade <= 79) {
+	document.write("Good! You achieved B+ grade")
+}
+else if (grade >= 70 && grade <= 74) {
+	document.write("Good! You achieved B- grade")
+}
+else if (grade >= 65 && grade <= 69) {
+	document.write("Good! You achieved B grade")
+}
+else if (grade >= 60 && grade <= 64) {
+	document.write("You achieved C+ grade. You can do better than this")
+}
+else if (grade >= 55 && grade <= 59) {
+	document.write("You achieved C grade. Put More efforts")
+}
+else if(grade >= 50 && grade <= 54) {
+	document.write("You achieved D grade. Needs Hardwork")
+}
+else if(grade >= 0 && grade <= 49) {
+	document.write("You achieved F grade. Attempt again and do more hardwork")
+}
